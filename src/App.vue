54a688801523fea01f6740404a9d53b25d46c0ac
adxell <adxell02@gmail.com>
2023-07-24 21:37:28 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+ <div class="wapper">
+  <h1>App.vue</h1>
+ </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import HomePage from '@/shared/pages/HomePage.vue'
+import AboutPage from '@/shared/pages/AboutPage.vue'
+
+const router = createRouter({
+    history: createWebHashHistory(import.meta.env.BASE_URL),
+    routes: [
+        { path: '/', name: 'home', component: HomePage },
+        { path: '/about', name: 'about', component: AboutPage },
+
+        {
+            path: '/characters',
+            name: 'characters',
+            component: () => import('@/chracters/layout/CharacterLayout.vue')
+        },
+        { path: '/:pathMatch(.*)*', redirect: () => ({ name: 'home' }) }
+    ]
+})
+
+export default router
